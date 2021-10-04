c5a113a89ca50a7420cb46ece6d2507f29e9671b
snordale <snordale@gmail.com>
2021-10-04 14:20:09 -0700

    fixed bot

--- bot.py
+++ bot.py
@@ -1,19 +1,47 @@
-import os, shutil
+import os, requests, tweepy
 
 from datetime import date, timedelta
 
-source_dir = '/Users/samNordale/calvinBotHeroku/comics'
-target_dir = '/Users/samNordale/calvinBotHeroku/all_comics'
 
-single_digit_strings = ["dead", "01", "02", "03", "04", "05", "06", "07", "08", "09"]
+INTERVAL = 60 * 60 * 24
 
-for year in range(1985, 1996):
-    for month in range(1, 13):
-        for day in range(1, 32):
-            day_string = single_digit_strings[day] if day < 10 else str(day)
-            month_string = single_digit_strings[month] if month < 10 else str(month)
-            filename = f'{source_dir}/{year}/{month_string}/{year}{month_string}{day_string}.gif'
-            try:
-                shutil.move(filename, target_dir)
-            except Exception as e:
-                print(e)
+CONSUMER_KEY=os.environ['CONSUMER_KEY']
+CONSUMER_SECRET=os.environ['CONSUMER_SECRET']
+ACCESS_SECRET=os.environ['ACCESS_SECRET']
+ACCESS_KEY=os.environ['ACCESS_KEY']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+def get_prev_comic():
+    with open('prev_comic.txt', 'r') as file:
+        return file.read(8)
+
+def set_prev_comic(filename):
+    with open('prev_comic.txt', 'r+') as file:
+        return file.write(filename)
+
+def tweet_next_comic():
+    prev_comic = get_prev_comic()
+
+    if prev_comic:
+        year = int(prev_comic[0:4])
+        month = int(prev_comic[4:6])
+        day = int(prev_comic[6:8])
+        new_date = date(year=year, month=month, day=day) + timedelta(days=1)
+    else:
+        new_date = date(year=1985, month=11, day=18)
+
+    new_day = new_date.strftime('%d')
+    new_filename = f'{new_date.year}{new_date.month}{new_day}.gif'
+    url = 'https://raw.githubusercontent.com/snordale/CalvinTwitterBot/master/comics/1995/01/19950101.gif?token=AFPGVQOMWX6CT4VI76VBJZLBMSNR4'
+    
+    filename = f'comics/{new_date.year}/{new_date.month}/{new_filename}'
+
+    media = api.media_upload(filename=file, chunked=True)
+    text = f'{new_date.month}/{new_day}/{new_date.year}'
+    
+    api.update_status(status=text, media_ids=[media.media_id])
+
+    set_prev_comic(f'{new_date.year}{new_date.month}{new_day}')
