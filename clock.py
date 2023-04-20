6b221fc7013b900778e8dbe8f5e697c757b65aec
Sam Nordale <sam@fairsquaremedicare.com>
2023-04-20 17:40:09 -0400

    follow job

--- clock.py
+++ clock.py
@@ -1,6 +1,6 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from bot import tweet_next_comic
+from bot import tweet_next_comic, follow_users
 
 
 sched = BlockingScheduler()
@@ -9,5 +9,10 @@ sched = BlockingScheduler()
 def scheduled_job():
     print("Tweeting next comic...")
     tweet_next_comic()
+
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=15)
+def scheduled_job():
+    print("Following users...")
+    follow_users()
 
 sched.start()
