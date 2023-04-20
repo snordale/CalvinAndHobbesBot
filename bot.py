65e2c43a4e7e9a6f6018a0ee8b9065513d87984e
Sam Nordale <sam@fairsquaremedicare.com>
2023-04-20 17:40:09 -0400

    follow job

--- bot.py
+++ bot.py
@@ -4,17 +4,50 @@ from datetime import datetime
 
 from comic_dict import comic_dict
 
+PROD = os.environ.get('PROD') == 'true'
 
-CONSUMER_KEY=os.environ['CONSUMER_KEY']
-CONSUMER_SECRET=os.environ['CONSUMER_SECRET']
-ACCESS_SECRET=os.environ['ACCESS_SECRET']
-ACCESS_KEY=os.environ['ACCESS_KEY']
+if PROD:
+    # Production environment, load variables from os.environ
+    CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+    CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+    ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
+    ACCESS_KEY = os.environ.get('ACCESS_KEY')
+else:
+    # Development environment, load variables from .env file
+    from dotenv import load_dotenv
+    load_dotenv()
+    CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+    CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
+    ACCESS_SECRET = os.getenv('ACCESS_SECRET')
+    ACCESS_KEY = os.getenv('ACCESS_KEY')
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+
+twitterV1 = tweepy.API(auth)
+
+twitterV2 = tweepy.Client(
+    consumer_key=CONSUMER_KEY,
+    consumer_secret=CONSUMER_SECRET,
+    access_token=ACCESS_KEY, 
+    access_token_secret=ACCESS_SECRET
+    )
+
 
 first_day = datetime(2021, 10, 6)
+
+
+def follow_users():
+    targetUser = 'Calvinn_Hobbes'
+    # targetId = '1579422614'
+
+    followerIds = twitterV1.get_follower_ids(screen_name=targetUser)
+    for id in followerIds[50:60]:
+        try:
+            twitterV1.create_friendship(user_id=id)
+        except:
+            print('Unable to follow: ', id)
+
 
 
 def get_comic_index():
@@ -32,7 +65,14 @@ def tweet_next_comic():
 
     full_path = f'comics/{year}/{month}/{filename}'
 
-    media = api.media_upload(filename=full_path, chunked=True)
+    # if PROD:
+    media = twitterV1.media_upload(filename=full_path, chunked=True)
+    # else:
+    #     print('Upload Media from: ' + full_path)
+
     text = f'{int(month)}/{int(day)}/{int(year)}'
 
-    api.update_status(status=text, media_ids=[media.media_id])
+    # if PROD:
+    twitterV1.update_status(status=text, media_ids=[media.media_id])
+    # else:
+    #     print('Send Tweet w/ text: ' + text)
