d1dbd5a565e67696fda1f6d44a1d0be2bce8308b
Snordale <snordale@gmail.com>
2025-01-12 17:03:59 -0500

    Fix scheduler

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -2,15 +2,15 @@ import { CronJob } from 'cron';
 import { tweetNextComic, followUsers } from './bot';
 
 // Tweet comic every day at 16:00 (4 PM)
-new CronJob('0 16 * * *', async () => {
+new CronJob('0 9 * * *', async () => {
   console.log('Tweeting next comic...');
   await tweetNextComic();
-}, null, true);
+}, null, true, 'America/Los_Angeles');
 
 // Follow users every day at 15:00 (3 PM)
-new CronJob('0 15 * * *', async () => {
+new CronJob('0 9 * * *', async () => {
   console.log('Following users...');
   await followUsers();
-}, null, true);
+}, null, true, 'America/Los_Angeles');
 
 console.log('Scheduler started!'); 
